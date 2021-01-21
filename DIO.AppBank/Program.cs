410cf6bc3ce3cab320c8a6b59cab02739e7265bb
Danilson Sanches <dsanches06@outlook.com>
2021-01-21 02:27:40 +0000

    Criação de projeto e implementação da Classe Conta e Tipo Enumerado denominado TipoConta

---
+++ DIO.AppBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using DIO.AppBank.Model;
+using DIO.AppBank.Model.Enum;
+using System;
+
+namespace DIO.AppBank
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Danilson Sanches");
+            minhaConta.Depositar(100);
+            Console.WriteLine(minhaConta.ToString());
+        }
+    }
+}
